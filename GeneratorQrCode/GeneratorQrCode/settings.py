7f0aab77626038b6220e6cef8e699e4568519642
YuliiaKavunenko <crazycel.hihi@gmail.com>
2025-01-12 12:32:06 +0200

    update html for generatot page and my qrcodes page

--- GeneratorQrCode/GeneratorQrCode/settings.py
+++ GeneratorQrCode/GeneratorQrCode/settings.py
@@ -122,8 +122,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static"
+    BASE_DIR / "static",
+    BASE_DIR / 'generator_page/static'
 ]
+
 
 
 # Default primary key field type
